7a78ca91b5601e4b20372835b29b8ce2e7f7fbcc
nullenvk <nullenvk@protonmail.com>
2021-12-27 21:02:25 +0100

    Add arduino-cli build scripts

---
+++ firmware/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+arduino-cli compile --fqbn SparkFun:avr:promicro:cpu=16MHzatmega32U4 C2VR-Arduino
---
+++ firmware/upload.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+arduino-cli upload -p /dev/ttyACM0 --fqbn SparkFun:avr:promicro:cpu=16MHzatmega32U4 C2VR-Arduino
