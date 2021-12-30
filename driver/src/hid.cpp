959e5467535cc02a96d3ffb610d3123f0b6202a2
lxeiqr <lxeiqr@protonmail.com>
2021-12-30 23:39:53 +0100

    Complete HID handler

--- driver/src/hid.cpp
+++ driver/src/hid.cpp
@@ -1,33 +1,66 @@
 #include "hid.h"
+#include <hidapi/hidapi.h>
+#include <cstring>
 
 void CHIDHandler::thread_loop() {
     while(!b_shouldQuit) {
-        // TODO: Read HID data, lock the mutex, write quat, unlock mutex
+        uint8_t buf[17] = {0};
+        
+        // Request state 
+        buf[0] = 0x0;
+        buf[0] = 0x81;
+        hid_write(dev, buf, 17);
+
+        // Read requested state
+        hid_read_timeout(dev, buf, 17, HID_TIMEOUT);
+
+        int32_t tq[4];
+        memcpy(tq, buf+1, 16);
+
+        // Critical section
+        mutex.lock();
+
+        quat.x = (double)tq[0] / IMU_SCALE;
+        quat.y = (double)tq[1] / IMU_SCALE;
+        quat.z = (double)tq[2] / IMU_SCALE;
+        quat.w = (double)tq[3] / IMU_SCALE;
+
+        // End of critical section
+        mutex.unlock();
     }
 }
 
-void CHIDHandler::start(std::string_view vid, std::string_view pid) {
-    hmd_vid = vid.substr(0, 4);
-    hmd_pid = pid.substr(0, 4);
+bool CHIDHandler::start(unsigned short vid, unsigned short pid) {
+    this->vid = vid;
+    this->pid = pid;
+    
+    hid_init();
+    dev = hid_open(vid, pid, nullptr);
+
+    if(dev == nullptr)
+        return true;
 
     thrd.reset(new std::thread(&CHIDHandler::thread_loop, this));
+
+    return false;
 }
 
 void CHIDHandler::stop() {
     b_shouldQuit = true;
     thrd->join();
+    
+    hid_close(dev);
+    hid_exit();
+
     thrd.reset(nullptr);
 }
 
 vr::HmdQuaternion_t CHIDHandler::getHMDQuat() {
-    vr::HmdQuaternion_t q;
+    vr::HmdQuaternion_t r;
+    mutex.lock(); 
 
-    q.x = 1;
-    q.y = 0;
-    q.z = 0;
-    q.w = 0;
+    r = quat;
 
-    // TODO
-
-    return q;
+    mutex.unlock(); 
+    return r;
 }
--- driver/src/hid.h
+++ driver/src/hid.h
@@ -3,20 +3,25 @@
 #include <thread>
 #include <mutex>
 #include <openvr/openvr_driver.h>
+#include <hidapi/hidapi.h>
+
+const int HID_TIMEOUT = 8000;
+const double IMU_SCALE = 10000;
 
 class CHIDHandler {
 private:
-    std::string hmd_vid, hmd_pid;
+    unsigned short vid, pid;
 
     std::unique_ptr<std::thread> thrd = nullptr;
     std::mutex mutex;
     vr::HmdQuaternion_t quat;
 
     bool b_shouldQuit = false;
+    hid_device *dev;
 
     void thread_loop();
 public:
-    void start(std::string_view vid, std::string_view pid);
+    bool start(unsigned short vid, unsigned short pid);
     void stop();
     vr::HmdQuaternion_t getHMDQuat();
 };
